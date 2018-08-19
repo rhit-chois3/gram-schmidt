02a65b871a8e9d522d69ff64de35554909c231a3
Joel Legg <joel.legg@libera.com>
2018-08-18 23:48:55 -0400

    removed redundancy

--- gramSchmidt.js
+++ gramSchmidt.js
@@ -6,36 +6,34 @@ var orthoNormVecs = [];
 gramSchmidt(matrix[0], 0);
 
 function gramSchmidt(vector, i) {
-    while (i < matrix.length) {
-        // var currentVec = matrix[i];
-        var currentVec = vector;
-        var orthoVec = [];
+    // var currentVec = matrix[i];
+    var currentVec = vector;
+    var orthoVec = [];
 
-        // orthogonalization
-        for (let j = 0; j < i; j++) {
-            var prevVectorNorm = orthoNormVecs[j];
-            var dotProd = getDotProd(prevVectorNorm, currentVec);
-            var resVec = multiply(dotProd, prevVectorNorm);
-            orthoVec = subtract(resVec, currentVec);
-        }
+    // orthogonalization
+    for (let j = 0; j < i; j++) {
+        var prevVectorNorm = orthoNormVecs[j];
+        var dotProd = getDotProd(prevVectorNorm, currentVec);
+        var resVec = multiply(dotProd, prevVectorNorm);
+        orthoVec = subtract(resVec, currentVec);
+    }
 
-        var vector = [];
-        if (orthoVec.length != 0) {
-            vector = orthoVec;
-        } else {
-            vector = currentVec;
-        }
+    var vector = [];
+    if (orthoVec.length != 0) {
+        vector = orthoVec;
+    } else {
+        vector = currentVec;
+    }
 
-        // normalization
-        var multiple = Math.sqrt(getDotProd(vector, vector));
-        var normVec = multiply((1/multiple), vector);
-        orthoNormVecs.push(normVec);
+    // normalization
+    var multiple = Math.sqrt(getDotProd(vector, vector));
+    var normVec = multiply((1/multiple), vector);
+    orthoNormVecs.push(normVec);
 
-        if ((i + 1) != matrix.length) {
-            return gramSchmidt(matrix[i + 1], (i + 1))
-        } else {
-            return logResult();
-        }
+    if ((i + 1) != matrix.length) {
+        return gramSchmidt(matrix[i + 1], (i + 1))
+    } else {
+        return logResult();
     }
 }
 
